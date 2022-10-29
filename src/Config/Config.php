c1be61955cda5c10a5c11427d9853f8e64bb09f4
eneassena <desenvolvedoreneas2000@gmail.com>
2022-10-28 23:26:53 -0300

    read headers of requests

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -1,6 +1,7 @@
 <?php
 
 define('baseUrl', 'http://localhost/slimframework2.0');
+define('port', 88888);
 
 
  
---
+++ src/Config/Route.php
@@ -0,0 +1,42 @@
+<?php 
+
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+
+$app->add(function(Request $request, RequestHandler $handler){
+    $headers = $request->getHeaders();
+    foreach($headers as $name => $values) {
+        echo "<p>".$name.":".implode(", ", $values)."</p>";
+    }
+
+    // obtem um cabeçario
+    $accept = $request->getHeader("Accept");
+    echo "<p>Header Accept:</p>";
+    $accept = explode(",", implode(',', $accept));
+    foreach ($accept as $key => $value) {
+        print "<p>{$key}:{$value}</p>";
+    }
+
+
+    return $handler->handle($request);
+});
+
+
+$app->get('/', function (Request $request, Response $response) {
+    $message = "App SlimFramework 4.0";
+    echo "<p>function</p>";
+    $response->getBody()->write($message);
+    
+    return $response;
+});
+
+
+
+
+$app->run();
